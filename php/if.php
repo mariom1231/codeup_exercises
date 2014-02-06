3a5be81398f9a8bc3987f3b42970ee03f3f45fb1
mariom1231 <mamoren1@gmail.com>
2014-02-06 11:56:44 -0600

    Completed exercise on If
    Codeup rocks!

---
+++ php/if.php
@@ -0,0 +1,46 @@
+<?php
+
+
+$a = 5;
+$b = 10;
+$c = '10';
+
+if ($a == $b) {echo "$a is equal to $b";}
+
+if ($a != $b) {echo "$a is not equal to $b";}
+
+if ($a < $b) { 
+	// output the appropriate result
+	echo "$a is less than $b\n";
+}
+
+if ($b > $a) {
+	// output the appropriate result
+	echo "$b is greater than $a\n";
+}
+if ($b >= $c) {
+	// output the appropraiate result
+	echo "$b is greater than or equal to $c\n"; 
+}
+if ($b <= $c) {
+	// output the appropraiate result
+	echo "$b is less than or $c\n";
+}
+if ($b == $c) {
+	// output the appropriate result
+	echo "$b is equal to $c\n";
+}
+if ($b === $c) {
+	// output the appropriate result
+	echo "$b is identical to $c\n";
+}
+//output the appropriate result
+if "$b is != to $c\n"; {
+
+}
+// output the appropriate result
+if ($b !== $c) {
+	echo "$b is not identical to $c\n";
+
+?>
+} 
