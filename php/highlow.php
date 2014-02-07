505732bb97080584ee28f0f276e7af80fa19b857
mariom1231 <mamoren1@gmail.com>
2014-02-07 14:18:59 -0600

    correct but tweaking
    change echo for readabillity

--- php/highlow.php
+++ php/highlow.php
@@ -1,23 +1,42 @@
 <?php
+
 $rand = mt_rand(1,100);
+$guesses = 0;
+
 
 // Prompt user to guess a number.
-fwrite(STDOUT, 'Guess a number between 1 and 100?');
+fwrite(STDOUT, "Guess a number between 1 and 100?\n");
 
 // Get the input from the user.
 $number = fgets(STDIN);
 
 // Output the user's response.
 // If lower than correct response, ask user to guess higher.
-while if ($number < $rand) {  
-	fwrite(STDOUT, 'Guess higher');
-}
+while ($number != $rand) {
+
+	if ($number < $rand) {  
+		fwrite(STDOUT, "Guess higher\n");
+	}
 // If lower than correct response, ask user to guess lower.
-if ($number > $rand) {
-	fwrite(STDOUT, 'Guess lower');
-}
+	elseif ($number > $rand) {
+		fwrite(STDOUT, "Guess lower\n");
+	}
 // If correct, say, Great job!
-if ($number == $rand) {
-	fwrite(STDOUT, "Great job!");
+	else {
+		fwrite(STDOUT, "Great job! You are awesome!\n");
+	}
+
+	// Prompt user to guess a number.
+	fwrite(STDOUT, "Wrong. Try Again.\n");
+
+	// Get the input from the user.
+	$number = fgets(STDIN);
+
+	$guesses++;
 }
+
+	echo "It took you $guesses guesses to get the answer\n";
+
 exit(0);
+
+?>
