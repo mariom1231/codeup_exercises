19a11ca6559d0b21afcdcfd31ce94d33038848f0
mariom1231 <mamoren1@gmail.com>
2014-02-07 17:59:06 -0600

    combined some if statements

--- php/if.php
+++ php/if.php
@@ -7,36 +7,37 @@ $c = '10';
 
 if ($a < $b) { 
 	echo "$a is less than $b\n";
-} elseif ($a > $b) {
+} else {
 	echo "$a is greater than $b\n";
 }
 
-if ($b > $a) {
+if($b > $a) {
 	echo "$b is greater than $a\n";
-} elseif ($b < $a)
-	echo ("$b is less than $a\n");
+} else {
+	echo "$b is less than $a\n";
+}
 
 if ($b >= $c) {
 	echo "$b is greater than or equal to $c\n"; 
-} elseif($b <= $c) {
+} else {
 	echo "$b is less than or equal to $c\n";
 }
 
-if ($b <= $c) {
-	echo "$b is less than or equal to $c\n";
-} elseif($b >= $c) {
-	echo "$b is greater than or equal to $c\n";
+if ($c >= $b) {
+	echo "$c is greater than or equal to $b\n"; 
+} else {
+	echo "$c is less than or equal to $b\n";
 }
 
 if ($b == $c) {
 	echo "$b is equal to $c\n";
-} elseif($b !== $c) {
+} else {
 	echo "$b is not equal to $c\n";
 }
 
 if ($b === $c) {
 	echo "$b is identical to $c\n";
-} elseif ($b !== $c) {
+} else {
 	echo "$b is not identical to $c\n";
 }
 
