fa271bcc4deaea73bffb28e19ecf8d3e3b245517
mariom1231 <mamoren1@gmail.com>
2014-02-10 08:59:29 -0600

    corrected bracket issues

--- php/fizzbuzz.php
+++ php/fizzbuzz.php
@@ -5,20 +5,19 @@ for ($i = 1; $i <= 100; $i++) {
 
 	if ($i % 3 == 0 && $i % 5 == 0){
 
-	}
 		echo "FIZZBUZZ!\n";
-
-	elseif ($i % 3) {
-	
 	}
+	elseif ($i % 3 == 0) {
+	
 		echo "FIZZ!\n";
-
+	}
 	elseif ($i % 5 == 0) {
 	
-		echo "BUZZ!";
+		echo "BUZZ!\n";
 	}
 	else {
 		echo "$i\n";
 	}
 
+}
 ?>
