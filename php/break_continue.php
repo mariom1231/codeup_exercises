26891bc2c0488505b2fd461c8d948a651fb5ddfb
mariom1231 <mamoren1@gmail.com>
2014-02-10 14:55:15 -0600

    changed operator in if statement

--- php/break_continue.php
+++ php/break_continue.php
@@ -2,9 +2,22 @@
 
 for ($i = 1; $i <= 100; $i++) {
     
-    if ($i % 2 == 0) {
-     echo $i . "\n";
+    if ($i % 2 !== 0) {
         continue;
     }
+    	echo $i . "\n";
     
+}
+
+?>
+
+
+<?php
+
+for ($i = 1; $i <= 100; $i++) {
+    
+    if ($i <= 10) {
+    	echo $i . "\n";
+        break;
+    }
 }
