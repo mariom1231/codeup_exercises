887f4f00ae8f8a24101dcf1c5c891389c42eb966
mariom1231 <mamoren1@gmail.com>
2014-02-10 13:03:44 -0600

    corrected $thing, added else if lines

--- php/foreach.php
+++ php/foreach.php
@@ -1,10 +1,14 @@
 <?php
 
-$data = array(42, 'bacon', (6 * 3), 'The Big Bang Theory', 98.6);
-foreach ($data as $datum) {
-	if (is_numeric($datum)) {
-		echo "{$datum} is a number\n";
-	} elseif (is_string($datum)) {
-		echo "{datum} is a string\n";
+$things = array('Sgt. Pepper', "11", null, array(1, 2, 3), 3.14, "12 + 7", false, (string) 11);
+foreach ($things as $thing) {
+	if (is_array($thing)) {
+		echo "{$thing} is a array\n";
+	} elseif (is_bool($thing)) {
+		echo "{$thing} is a bool\n";
+	} elseif (is_float($thing)) {
+		echo "{$thing} is a float\n";
+	} elseif (is_int($thing)) {
+		echo "{$thing} is an int\n";
 	}
 }
