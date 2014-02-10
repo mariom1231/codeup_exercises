2e41ab9cb6f5a6caa25fe60dfc584f352bd55886
mariom1231 <mamoren1@gmail.com>
2014-02-09 19:39:21 -0600

    success
    finally saw the errors and made corrections

--- php/if.php
+++ php/if.php
@@ -7,40 +7,34 @@ $c = '10';
 
 if ($a < $b) { 
 	echo "$a is less than $b\n";
-} else {
-	echo "$a is greater than $b\n";
 }
 
 if($b > $a) {
 	echo "$b is greater than $a\n";
-} else {
-	echo "$b is less than $a\n";
 }
 
 if ($b >= $c) {
 	echo "$b is greater than or equal to $c\n"; 
-} else {
-	echo "$b is less than or equal to $c\n";
 }
 
-if ($c >= $b) {
-	echo "$c is greater than or equal to $b\n"; 
-} else {
-	echo "$c is less than or equal to $b\n";
+if ($b <= $c) {
+	echo "$c is less than or equal to $b\n"; 
 }
 
 if ($b == $c) {
 	echo "$b is equal to $c\n";
-} else {
-	echo "$b is not equal to $c\n";
 }
 
 if ($b === $c) {
 	echo "$b is identical to $c\n";
-} else {
+}
+
+if ($b != $c) {
+	echo "$b is not equal to $c\n";
+}
+
+if ($b !== $c) {
 	echo "$b is not identical to $c\n";
 }
 
 ?>
-
-
