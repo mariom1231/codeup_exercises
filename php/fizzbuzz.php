42a0ea2b4e60a2a17c9c100c288763f8217e9935
mariom1231 <mamoren1@gmail.com>
2014-02-10 03:06:30 -0600

    parse error line 11

--- php/fizzbuzz.php
+++ php/fizzbuzz.php
@@ -1,5 +1,24 @@
 <?php
 // Expr1 is the initializer, Expr2 is the condition, Expr3 is the implementation.
-for ($i = 0; $i <= 100; $i += 2) {
-	echo "\$i has a value of {$i}\n";
-}
+for ($i = 1; $i <= 100; $i++) {
+	
+
+	if ($i % 3 == 0 && $i % 5 == 0){
+
+	}
+		echo "FIZZBUZZ!\n";
+
+	elseif ($i % 3) {
+	
+	}
+		echo "FIZZ!\n";
+
+	elseif ($i % 5 == 0) {
+	
+		echo "BUZZ!";
+	}
+	else {
+		echo "$i\n";
+	}
+
+?>
