85092a8c2096b423563d747312af1c51056b5e89
mariom1231 <mamoren1@gmail.com>
2014-02-09 22:29:21 -0600

    removed 3 duplicate IF statements. now have 7 IF statements as instructed in example. correct output.

--- php/if_logical.php
+++ php/if_logical.php
@@ -42,19 +42,5 @@ if ($y > 0 && $y < 10) {
 if ($z > 0 && $z < 10) {
 	echo "$z is greater than 0 AND less than 10\n";
 }
-// If $x is greater than 0 AND less than 10, echo the result as a sentence "$x is greater than 0 AND less than 10".
-
-if ($x > 0 && $x < 10) {
-	echo "$x is greater than 0 AND less than 10\n";
-} 
-// repeat the if statement for $y and $z.
-
-if ($y > 0 && $y < 10) {
-	echo "$y is greater than 0 AND less than 10\n";
-}
-
-if ($z > 0 && $z < 10) {
-	echo "$z is greater than 0 AND less than 10\n";
-}
 
 ?>
