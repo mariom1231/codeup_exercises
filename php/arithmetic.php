12851d81f9be9b47b07b51489209134f080684c8
mariom1231 <mamoren1@gmail.com>
2014-02-11 15:16:58 -0600

    corrected validate statements

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -9,39 +9,38 @@ function validate($num1, $num2) {
 }
 
 function add($num1, $num2) {
-   validate($num1, $num2);
-		echo $num1 + $num2 . PHP_EOL;
-	
+	if (validate($num1, $num2)) {
+		return $num1 + $num2 . PHP_EOL;
+	}
 }
 
 function subtract($num1, $num2) {
-   validate($num1, $num2);
-		echo $num1 - $num2 . PHP_EOL
+   if (validate($num1, $num2)) {
+		return $num1 - $num2 . PHP_EOL;
+	}
 }
-
 function multiply($num1, $num2) {
-   validate($num1, $num2);
-		echo $num1 * $num2 . PHP_EOL
+   if (validate($num1, $num2)) {
+		return $num1 * $num2 . PHP_EOL;
+	}
 }
 
 function divide($num1, $num2) {
-   if (is_numeric($num1) && is_numeric($num2)) {
+   if (validate($num1, $num2)) {
+		return $num1 / $num2 . PHP_EOL;
 		if ($num2 == 0) {
-		echo "ERROR! You cannot divide by 0." . PHP_EOL;
-}		else echo $num1 / $num2 . PHP_EOL;
-	} else {
-		echo "ERROR! Both $num1 and $num2 should be numbers." . PHP_EOL;
-	}
+		} else {
+        return "FALSE" . PHP_EOL;
+    }    
 }
 
 function modulus($num1, $num2) {
-   if (is_numeric($num1) && is_numeric($num2)) {
+	if (validate($num1, $num2)) {
+		return $num1 % $num2 . PHP_EOL;
 		if ($num2 == 0) {
-		echo "ERROR! You cannot divide by 0." . PHP_EOL;
-}		else echo $num1 % $num2 . PHP_EOL;
-	} else {
-		echo "ERROR! Both $num1 and $num2 should be numbers." . PHP_EOL;
-	}
+		} else {
+        return "FALSE" . PHP_EOL;
+    }    
 }
 
 $num1 = 30;
@@ -53,6 +52,3 @@ multiply($num1, $num2);
 divide($num1, $num2);
 modulus($num1, $num2);
 
-?>
-
-	
