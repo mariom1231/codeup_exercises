afad3ce517531212005ef1190a93f758e19f077f
mariom1231 <mamoren1@gmail.com>
2014-02-11 16:28:53 -0600

    add initial text to file

---
+++ php/internal-functions.php
@@ -0,0 +1,17 @@
+<?php
+
+$nothing = NULL;
+$something = '';
+$array = array(1,2,3);
+
+// Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+
+// TEST: If var $nothing is set, display '$nothing is SET'
+
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+
+// TEST: If var $something is set, display '$something is SET'
+
+// Serialize the array $array, and output the results
+
+// Unserialize the array $array, and output the results
