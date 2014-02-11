448ddc10da88f091af545dd47dad33f19f02c33e
mariom1231 <mamoren1@gmail.com>
2014-02-11 14:00:27 -0600

    example 1

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -4,7 +4,7 @@ function add($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		echo $num1 + $num2 . PHP_EOL;
 	} else {
-		echo "ERROR! Both $num1 and $num2 should be numbers" . PHP_EOL;
+		echo "ERROR! Both $num1 and $num2 should be numbers." . PHP_EOL;
 	}
 }
 
@@ -12,7 +12,7 @@ function subtract($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		echo $num1 - $num2 . PHP_EOL;
 	} else {
-		echo "ERROR! Both $num1 and $num2 should be numbers" . PHP_EOL;
+		echo "ERROR! Both $num1 and $num2 should be numbers." . PHP_EOL;
 	}
 }
 
@@ -20,32 +20,32 @@ function multiply($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		echo $num1 * $num2 . PHP_EOL;
 	} else {
-		echo "ERROR! Both $num1 and $num2 should be numbers" . PHP_EOL;
+		echo "ERROR! Both $num1 and $num2 should be numbers." . PHP_EOL;
 	}
 }
 
 function divide($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		if ($num2 == 0) {
-		echo "ERROR! You cannot divide by 0" . PHP_EOL;
+		echo "ERROR! You cannot divide by 0." . PHP_EOL;
 }		else echo $num1 / $num2 . PHP_EOL;
 	} else {
-		echo "ERROR! Both $num1 and $num2 should be numbers" . PHP_EOL;
+		echo "ERROR! Both $num1 and $num2 should be numbers." . PHP_EOL;
 	}
 }
 
 function modulus($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		if ($num2 == 0) {
-		echo "ERROR! You cannot divide by 0" . PHP_EOL;
+		echo "ERROR! You cannot divide by 0." . PHP_EOL;
 }		else echo $num1 % $num2 . PHP_EOL;
 	} else {
-		echo "ERROR! Both $num1 and $num2 should be numbers" . PHP_EOL;
+		echo "ERROR! Both $num1 and $num2 should be numbers." . PHP_EOL;
 	}
 }
 
-$num1 = 'cat';
-$num2 = 0;
+$num1 = 30;
+$num2 = 12;
 
 add($num1, $num2);
 subtract($num1, $num2);
---
+++ php/func_examples.php
@@ -0,0 +1,10 @@
+<?php
+
+is_numeric(11);
+
+function add($a, $b) {
+	echo $a + $b . PHP_EOL;
+
+}
+
+add(4,3);
