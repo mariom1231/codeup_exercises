401ba62246dfda1fd75e96859ab9f0f338451955
mariom1231 <mamoren1@gmail.com>
2014-02-11 10:46:37 -0600

    example 2

--- php/functions_exer.php
+++ php/functions_exer.php
@@ -1,7 +1,13 @@
 <?php
 
-$sum = (20 + 22) . PHP_EOL;
-$sum2 = (35 + 7) . PHP_EOL; 
-echo $sum;
-echo $sum2;
+function add($num1, $num2) {
+	echo ($num1 + $num2) . PHP_EOL;
+}
+
+add(30, 2);
+//$sum = (20 + 22) . PHP_EOL;
+//$sum2 = (35 + 7) . PHP_EOL; 
+//echo $sum;
+//echo $sum2;
+
 
