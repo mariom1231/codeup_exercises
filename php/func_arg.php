f84790018a70a126be12c673f93f977fa1058bf6
mariom1231 <mamoren1@gmail.com>
2014-02-11 11:56:35 -0600

    tried to add array, rec’d fatal error

--- php/func_arg.php
+++ php/func_arg.php
@@ -1,18 +1,18 @@
 <?php 
 
-//function add($num1, $num2 = 0) {
-//    echo ($num1 + $num2) . "\n";
-//}
-
-
-// echo add ();
-
-function trim_with_new_line($item, $newline = TRUE) {
-	if ($newline === TRUE) {
-		echo trim($item) . PHP_EOL;
-	} else {
-		echo trim($item);
-	}
+function add($num1, $num2 = 0) {
+   echo ($num1 + $num2) . "\n";
 }
 
-trim_with_new_line('    Jason    ');
+
+add (array(), 'Jason');
+
+// function trim_with_new_line($item, $newline = TRUE) {
+// 	if ($newline === TRUE) {
+// 		echo trim($item) . PHP_EOL;
+// 	} else {
+// 		echo trim($item);
+// 	}
+// }
+
+// trim_with_new_line('    Jason    ');
