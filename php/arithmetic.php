36736b8feca07695c153733896e3b7f386ee9ea8
mariom1231 <mamoren1@gmail.com>
2014-02-11 13:40:51 -0600

    error msgs now show the value of $num1 and $num2

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -12,7 +12,7 @@ function subtract($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		echo $num1 - $num2 . PHP_EOL;
 	} else {
-		echo "ERROR! Both args should be numbers" . PHP_EOL;
+		echo "ERROR! Both $num1 and $num2 should be numbers" . PHP_EOL;
 	}
 }
 
@@ -20,7 +20,7 @@ function multiply($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		echo $num1 * $num2 . PHP_EOL;
 	} else {
-		echo "ERROR! Both args should be numbers" . PHP_EOL;
+		echo "ERROR! Both $num1 and $num2 should be numbers" . PHP_EOL;
 	}
 }
 
@@ -30,7 +30,7 @@ function divide($num1, $num2) {
 		echo "ERROR! You cannot divide by 0" . PHP_EOL;
 }		else echo $num1 / $num2 . PHP_EOL;
 	} else {
-		echo "ERROR! Both args should be numbers" . PHP_EOL;
+		echo "ERROR! Both $num1 and $num2 should be numbers" . PHP_EOL;
 	}
 }
 
@@ -40,7 +40,7 @@ function modulus($num1, $num2) {
 		echo "ERROR! You cannot divide by 0" . PHP_EOL;
 }		else echo $num1 % $num2 . PHP_EOL;
 	} else {
-		echo "ERROR! Both args should be numbers" . PHP_EOL;
+		echo "ERROR! Both $num1 and $num2 should be numbers" . PHP_EOL;
 	}
 }
 
