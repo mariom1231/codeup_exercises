c0bbb7ae0f983688e60fb965ecfa93d50b6a196c
mariom1231 <mamoren1@gmail.com>
2014-02-11 11:05:39 -0600

    verified output for each

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,19 +1,26 @@
 <?php
 
-function add($a, $b) {
-    echo $a + $b;
+function add($num1, $num2) {
+    echo $num1 + $num2 . PHP_EOL;
 }
 
-function subtract($a, $b) {
-    echo $a - $b;
+function subtract($num1, $num2) {
+    echo $num1 - $num2 . PHP_EOL;
 }
 
-function multiply($a, $b) {
-    echo $a - $b;
+function multiply($num1, $num2) {
+    echo $num1 - $num2 . PHP_EOL;
 }
 
-function divide($a, $b) {
-    echo $a / $b;
+function divide($num1, $num2) {
+    echo $num1 / $num2 . PHP_EOL;
 }
 
+$num1 = 30;
+$num2 = 12;
+
+add($num1, $num2);
+subtract($num1, $num2);
+multiply($num1, $num2);
+divide($num1, $num2);
 ?>
