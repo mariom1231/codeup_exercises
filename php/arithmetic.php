2533efe478552e933373965a44c2ad67c9700036
mariom1231 <mamoren1@gmail.com>
2014-02-11 13:39:02 -0600

    changed the echo statements from ‘ to “

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -4,7 +4,7 @@ function add($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		echo $num1 + $num2 . PHP_EOL;
 	} else {
-		echo 'ERROR! Both args should be numbers' . PHP_EOL;
+		echo "ERROR! Both $num1 and $num2 should be numbers" . PHP_EOL;
 	}
 }
 
@@ -12,7 +12,7 @@ function subtract($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		echo $num1 - $num2 . PHP_EOL;
 	} else {
-		echo 'ERROR! Both args should be numbers' . PHP_EOL;
+		echo "ERROR! Both args should be numbers" . PHP_EOL;
 	}
 }
 
@@ -20,31 +20,31 @@ function multiply($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		echo $num1 * $num2 . PHP_EOL;
 	} else {
-		echo 'ERROR! Both args should be numbers' . PHP_EOL;
+		echo "ERROR! Both args should be numbers" . PHP_EOL;
 	}
 }
 
 function divide($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		if ($num2 == 0) {
-		echo 'ERROR! You cannot divide by 0' . PHP_EOL;
+		echo "ERROR! You cannot divide by 0" . PHP_EOL;
 }		else echo $num1 / $num2 . PHP_EOL;
 	} else {
-		echo 'ERROR! Both args should be numbers' . PHP_EOL;
+		echo "ERROR! Both args should be numbers" . PHP_EOL;
 	}
 }
 
 function modulus($num1, $num2) {
    if (is_numeric($num1) && is_numeric($num2)) {
 		if ($num2 == 0) {
-		echo 'ERROR! You cannot divide by 0' . PHP_EOL;
+		echo "ERROR! You cannot divide by 0" . PHP_EOL;
 }		else echo $num1 % $num2 . PHP_EOL;
 	} else {
-		echo 'ERROR! Both args should be numbers' . PHP_EOL;
+		echo "ERROR! Both args should be numbers" . PHP_EOL;
 	}
 }
 
-$num1 = 30;
+$num1 = 'cat';
 $num2 = 0;
 
 add($num1, $num2);
