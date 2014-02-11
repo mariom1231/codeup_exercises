01c3589ed3c11bb850b4e261619302de56108a28
mariom1231 <mamoren1@gmail.com>
2014-02-11 11:51:20 -0600

    example 4

--- php/func_arg.php
+++ php/func_arg.php
@@ -1,6 +1,18 @@
 <?php 
-function add($num1 = 10, $num2 = 10) {
-    echo ($num1 + $num2) . "\n";
+
+//function add($num1, $num2 = 0) {
+//    echo ($num1 + $num2) . "\n";
+//}
+
+
+// echo add ();
+
+function trim_with_new_line($item, $newline = TRUE) {
+	if ($newline === TRUE) {
+		echo trim($item) . PHP_EOL;
+	} else {
+		echo trim($item);
+	}
 }
 
-echo add ();
+trim_with_new_line('    Jason    ');
