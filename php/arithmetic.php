32ecaaef02fe7c3143bb63e5ea1522717a81f394
mariom1231 <mamoren1@gmail.com>
2014-02-11 10:56:12 -0600

    exercise 1, add code blocks to each function

---
+++ php/arithmetic.php
@@ -0,0 +1,19 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b;
+}
+
+function subtract($a, $b) {
+    echo $a - $b;
+}
+
+function multiply($a, $b) {
+    echo $a - $b;
+}
+
+function divide($a, $b) {
+    echo $a / $b;
+}
+
+?>
