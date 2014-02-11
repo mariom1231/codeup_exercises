8cace3f61d65e653cd212933400351216cd2ac77
mariom1231 <mamoren1@gmail.com>
2014-02-11 12:04:22 -0600

    example 5

--- php/func_arg.php
+++ php/func_arg.php
@@ -1,11 +1,15 @@
 <?php 
 
 function add($num1, $num2 = 0) {
-   echo ($num1 + $num2) . "\n";
+   if (is_numeric($num1) && is_numeric($num2)) {
+	echo $num1 + $num2 . PHP_EOL;
+	} else {
+		echo ' Both args should be numbers' . PHP_EOL;
+	}
 }
 
 
-add (array(), 'Jason');
+add(3.14, TRUE);
 
 // function trim_with_new_line($item, $newline = TRUE) {
 // 	if ($newline === TRUE) {
