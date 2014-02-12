6d25c0fa99b14b7ebfe6c0b2765338aebf14fdf6
mariom1231 <mamoren1@gmail.com>
2014-02-12 07:18:06 -0600

    created functions to test for SET || EMPTY

--- php/internal-functions.php
+++ php/internal-functions.php
@@ -4,13 +4,31 @@ $nothing = NULL;
 $something = '';
 $array = array(1,2,3);
 
-// Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+// Both NULL and " " are considered EMPTY
+// array (1,2,3) is not considered EMPTY
 
 // TEST: If var $nothing is set, display '$nothing is SET'
 
+// function checkvalue($nothing, $something, $array) {
+		//echo;
+// }
+if (isset($nothing)) {
+	    echo "$nothing is SET";
 // TEST: If var $nothing is empty, display '$nothing is EMPTY'
-
+elseif (empty($nothing)) {
+		echo "$nothing is EMPTY";
+}
 // TEST: If var $something is set, display '$something is SET'
+if (isset($something)) {
+		echo "$something is SET";
+}
+
+
+
+
+
+
+
 
 // Serialize the array $array, and output the results
 
