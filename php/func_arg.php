94a125105ee21cf87eb0a78003d36709930fcbe7
mariom1231 <mamoren1@gmail.com>
2014-02-12 09:31:24 -0600

    var set with empty string

--- php/func_arg.php
+++ php/func_arg.php
@@ -1,18 +1,23 @@
 <?php 
 
-function add($num1, $num2 = 0) {
-   if (is_numeric($num1) && is_numeric($num2)) {
-	return $num1 + $num2 . PHP_EOL;
-	} else {
-		echo " ERROR! Both args should be numbers." . PHP_EOL;
-	}
-	echo "Done!";
-}
+$foo = "";
+
+var_dump(isset($foo));
 
 
-$result = add(TRUE, 3);
+// function add($num1, $num2 = 0) {
+//    if (is_numeric($num1) && is_numeric($num2)) {
+// 	return $num1 + $num2 . PHP_EOL;
+// 	} else {
+// 		echo " ERROR! Both args should be numbers." . PHP_EOL;
+// 	}
+// 	echo "Done!";
+// }
+
+
+// $result = add(TRUE, 3);
 	
-	echo $result . PHP_EOL;
+// 	echo $result . PHP_EOL;
 
 
 // function trim_with_new_line($item, $newline = TRUE) {
