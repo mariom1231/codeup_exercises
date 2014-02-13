d74c8ffdd5c140ff97500e8beac93938006eec0d
mariom1231 <mamoren1@gmail.com>
2014-02-13 16:14:55 -0600

    add semicolon

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -3,7 +3,7 @@
 // first names
 $names = ['Tina', 'Dana', 'Mike', "Amy", 'Adam'];
 
-$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael']
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 // Create a function that returns TRUE or FALSE if an array value is found.
 
@@ -22,3 +22,23 @@ function searchArrays($value, $array) {
 
 
 // Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
+
+// Use foreach to create function.
+// foreach $needles as $needles
+// 		return
+function count_matches($needles, $haystack){
+	$number_of_matches = 0;
+}
+	foreach ($needles as $needle) {
+		result = array_search($needle, $haystack);
+		if(is_numeric($numeric($result)) {
+			$number_of_matches++;
+		}
+	}
+	return $number_of_matches;
+
+	$names = ['Tina', 'Dana', 'Mike', "Amy", 'Adam'];
+
+	$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+	echo count_matches($compare, $names);
