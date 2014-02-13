2013c59a79cd16fb0398ea0107dd142338ae72d7
mariom1231 <mamoren1@gmail.com>
2014-02-13 14:33:47 -0600

    explode

---
+++ php/lists_with_commas.php
@@ -0,0 +1,33 @@
+<?php
+
+// Converts array into list n1, n2, ..., and n3
+function humanized_list($string) {
+  // Your solution goes here!
+	$array = explode(', ', $string);
+	$last_item = array_pop($array);
+
+	$newarray = implode(', ', $array) . ", and " . $last_item . PHP_EOL;
+	return $newarray;
+
+}
+
+
+
+// List of famous peeps
+
+$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+// Humanize that list
+$famous_fake_physicists = humanized_list($physicists_string);
+
+// Output sentence
+	echo $famous_fake_physicists;
+
+// $famous_fake_physicists = implode(', and [final item])', $physicists_string);
+// 	echo $famous_fake_physicists;
+// echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+
+//	echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+
+	
+?>
