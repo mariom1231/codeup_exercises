0a7c25f62ea5613a8142960c8e8a3545a2d32b23
mariom1231 <mamoren1@gmail.com>
2014-02-13 11:02:53 -0600

    example

---
+++ php/search_arrays_ex.php
@@ -0,0 +1,12 @@
+<?php
+
+$names = ['Marc Andreessen', 'Tim Berners-Lee', 'Len Bosack', 'Steve
+$Case', 'Vint Cerf', 'Len Kleinrock', 'J.C.R. Licklider', 'Bob
+$Metcalfe', 'Ray Tomlinson'];
+
+$result = array_search('Bill Gates', $names);
+var_dump($result);
+
+$result = array_Search('Bob Metcalfe', $names);
+var_dump($result);
+
