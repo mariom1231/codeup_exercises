81aa9ed367016cdf4d78adbc153c6cd8ec03dac3
mariom1231 <mamoren1@gmail.com>
2014-02-13 10:09:11 -0600

    class examples

---
+++ php/trim_ex.php
@@ -0,0 +1,36 @@
+<?php
+// TRIM EXAMPLE
+function get_input($upper = FALSE)
+{
+
+	if ($upper == TRUE {
+ 	  return strtoupper(trim(fgets(STDIN)));
+
+	} else {
+	  return trim(fgets(STDIN));
+	}
+}
+
+// function get_input($upper = FALSE)
+
+// 	$input = ($trim(fgets(STDIN)));
+
+//	if ($upper) {
+	// return strtoupper($input);
+
+//	} else {
+//		return $input;
+
+//	}
+
+}
+
+
+
+
+// SUM EXAMPLE
+function sum($a, $b = 0)
+{
+	return $a + $b;
+}
+
