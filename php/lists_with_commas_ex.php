c68da2f25fa2f35d8d113991b7f76f57a1a314d3
mariom1231 <mamoren1@gmail.com>
2014-02-13 14:32:21 -0600

    class examples

---
+++ php/lists_with_commas_ex.php
@@ -0,0 +1,7 @@
+<?php
+
+$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+$physicists_array = explode(', ', $physicists_string);
+
+echo $physicists_array;
