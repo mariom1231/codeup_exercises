bc753e49d0a288ea2748a9db233ee3cc092de476
mariom1231 <mamoren1@gmail.com>
2014-02-13 11:06:23 -0600

    push example

--- php/push_pop_shift_ex.php
+++ php/push_pop_shift_ex.php
@@ -1,13 +1,23 @@
 <?php
+// POP & PUSH for addding to the end of the array
 
-$array = [1,2,3,4,5];
+// $array = [1,2,3,4,5];
 
-$popped = array_pop($array);
+// $popped = array_pop($array);
 
-var_dump($popped);
+// var_dump($popped);
 
-var_dump($array);
+// var_dump($array);
 
-$popped2 = array_pop($array);
+// $popped2 = array_pop($array);
 
-var_dump($array);
+// var_dump($array);
+
+$items = ['First', 'Second', 'Third'];
+array_push($items, 'Fourth', 'Fifth', 'Sixth');
+print_r($items);
+
+
+// SHIFT & UNSHIFT for adding to the beginning of the array
+
+
