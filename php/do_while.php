2baddff5ac948226621efef4012f8756fc4e2834
mariom1231 <mamoren1@gmail.com>
2014-02-15 17:57:40 -0600

    Exercise 2
    reviewing class exercises; saving after each

--- php/do_while.php
+++ php/do_while.php
@@ -1,10 +1,22 @@
 <?php
+// Exercise 1: Create a do-while loop that will count by 2's starting with 0
+// and ending at 100. Follow each number with a newline.
 
-$a = 0;
+// $a = 0;
+
+// do {
+// 	echo "$a\n";
+// 	$a = $a + 2;
+	
+	
+// } while ($a <= 100);
+
+// Exercise 2: Alter your loop to count backwards by 5's from 100 to -10.
+
+$a = 100;
 
 do {
 	echo "$a\n";
-	$a = $a + 2;
-	
-	
-} while ($a <= 100);
+	$a = $a - 5;
+
+} while ($a >= -10);
