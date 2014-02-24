c1e3b1ddf47a99da61f82114f1dff0531ed41835
mariom1231 <mamoren1@gmail.com>
2014-02-24 08:41:46 -0600

    changed from if else to switch statements

--- codeup_todo_list/todo.php
+++ codeup_todo_list/todo.php
@@ -10,11 +10,10 @@ function list_items($list) {
     $string = '';
     
     foreach ($list as $key => $value) {
-        $items = $key + 1;
-        $string .= '[' . ($key + 1) . '] ' . $items . PHP_EOL;
-        
-        return $string;
-    }
+        $key++;
+        $string .=  "[{$key}] {$items} . PHP_EOL";
+    }    
+    return $string;
 }
 
 // // Get STDIN, strip whitespace and newlines, 
@@ -34,7 +33,6 @@ function read_file($filename) {
 
 // Output $contents_array
     return $contents_array;
-
 }
 
 function write_file($array) {
@@ -44,7 +42,20 @@ function write_file($array) {
     }
     
     fclose($handle);
+}
 
+function yesOrNo() {
+    echo 'Are you sure? (Y)es or (N)o? ';
+    $response = get_input(TRUE);
+    switch ($response) {
+        case 'Y':
+        return TRUE;
+        break;
+
+        default:
+        echo 'Last action cancelled. ';
+        break;
+    } 
 }
 
 // The loop!
@@ -55,83 +66,94 @@ do {
     
 
     // Show the menu options
-    echo '(O)pen file, (N)ew item, (R)emove item, (S)ave, (SO)rt, or (Q)uit : ';
+    echo '(N)ew item, (O)pen item, (R)emove item, (S)ave, (SO)rt, or (Q)uit : ';
 
     // Get the input from user
     $input = get_input(TRUE);
 
-    if ($input == 'O') {
-        echo 'Enter the filename. ';
+    switch ($input) {
+        
+        case 'N':
+
+        echo 'Enter item: ';
+
+        $addTo = get_input();
+    
+        echo 'Would you like to add this item to the (B)eginning or the (E)nd of the list? ';
+
+        $beg_or_end = get_input();
+        
+            if ($beg_or_end == 'B') {
+            array_unshift($items, $addTo);
+            
+            } else {
+            array_push($items, $addTo);
+        }        
+        break;
+
+        
+        case 'O':
+        
+        echo 'Enter the filepath and name. ';
         $filename = get_input();
         read_file($filename);
 
+        break;
+
+        case 'R':
+        
+        echo 'Enter item number to remove: ';
+        
+        // Get array key
+        $key = get_input();
+        $key--;
+        // Remove from array
+        unset($items[$key]);
+        $items = array($items);
+
+        echo 'Would you like to remove the (F)irst item or the (L)ast item from the list? ';
+
+        if($input == 'F') {
+            array_shift($items, 'remove first item ');
+            
+        } else {
+            array_pop($items, 'remove last item ');
+        }
+        break;
 // When (S)ave is chosen, the user should be able to enter the path to a file
 // to have it save. Use fwrite() with the mode that starts at the beginning of
 // a file and removes all the file contents, or creates a new one if it does
 // not exist. After save, alert the user the save was successful and redisplay
 // the list and main menu.
-        echo '(S)ave file? Enter (Y)es or (N)o. ';
+        case 'S':
 
-        if ($input == 'S') {
+        echo 'Enter the filepath to save to. ';
         $filename = get_input();
-        write_file($filename);
         
+        if(existing_file($filename)); {
+            echo 'This file already exists. Would you like to overwrite it? ';
+            $response = yesOrNo();
+
+            if($response) {
+                save_file($items, $filename);
+
+            } else {
+            save_file($items, $filename);
+        
+        echo "Save was successful.";
         }
-      
-        echo "Save was successful."
-    // // Redirect to main menu.
+        break;
 
-    } elseif ($input == 'N') {
-        echo 'Would you like to add this item to the (B)eginning or the (E)nd of the list? ';
-        $addTo = get_input(TRUE);
-        echo 'Enter item: ';
-        // Add entry to list array
-        $items[] = get_input();
-            }
-
-// Default to end if no input is given.
-// PUSH = add to END, UNSHIFT = add to BEG
-// If adding to (B) of the list, do this. Else, do this instead, add to the (E).
+        case 'SO':
         
-        if ($input == 'B') {
-            array_unshift($items, 'new first item ');
-            
-        } elseif ($input == 'E') {
-                array_push($items, 'new last item ');
-            }        
-
-
-    } elseif ($input == 'R') {
-        echo 'Enter item number to remove: ';
-        
-        // Get array key
-        $key = get_input();
-
-        // Remove from array
-        unset($items[$key - 1]);
-        $items = array($items);
-
-        echo 'Would you like to remove the (F)irst item or the (L)ast item from the list? ';
-
-    }
-
-        if($input == 'F') {
-            array_shift($items, 'remove first item ');
-            
-        } elseif ($input == 'L') {
-            array_pop($items, 'remove last item ');
-        }
-
-
-    } elseif ($input == 'S') {
-        // How do you want to sort?
         echo 'How would you like to sort: (A)-Z, or (Z)-A? ';
-        $sortBy = get_input(TRUE);
+        $sortBy = get_input();
         if ($sortBy == 'A') {
             $sort($items);
-        } elseif ($sortBy == 'Z') {
+        } else {
             rsort($items);
         }
+        break;
 
     }
 
--- php/highlow.php
+++ php/highlow.php
@@ -1,28 +1,33 @@
 <?php
 // Game picks a random number between 1 and 100.
-$number = mt_rand(1, 100);
+$rand = mt_rand(1, 100);
 
 // Prompt user to guess a number.
 fwrite(STDOUT, "Guess a number between 1 and 100?\n");
 
 // Get the input from the user.
 $number = fgets(STDIN);
-
+$count = 1;
 // Output the user's response.
 // If lower than correct response, ask user to guess higher.
 while ($number != $rand) {
 
-	if ($number < $rand) {  
+	if ($number < $rand)
+	 {  
+	
 		fwrite(STDOUT, "Guess HIGHER\n");
+		$number = fgets(STDIN);
 	}
 // If lower than correct response, ask user to guess lower.
 	elseif ($number > $rand) {
 		fwrite(STDOUT, "Guess LOWER\n");
-	}
+		$number = fgets(STDIN);
+	}	$count++;
+}
 // If correct, say, Great job!
-fwrite(STDOUT, "Great job! You are awesome!\n");
+fwrite(STDOUT, "Great job! You are awesome!!\n");
 
-fwrite(STDOUT, "It took you $guesses tries to get the correct answer.\n";
+fwrite(STDOUT, "It took you $count guesses to get the correct number.\n");
 
 exit(0);
 
