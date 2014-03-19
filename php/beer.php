1a4b0840bd7a781eb850a2a8531c62d404b949cb
mariom1231 <mamoren1@gmail.com>
2014-03-19 14:26:22 -0500

    Beer script

---
+++ php/beer.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 100;
+
+while ($a > 1) {
+$a--;
+$b = $a - 1;
+
+
+echo "$a bottles of beer on the wall, $a bottles of beer. Take one down, pass it around, $b bottles of beer on the wall.\n";
+
+} echo "No more bottles of beer on the wall, no more bottles of beer. Go to the store, buy some more, 99 bottles of beer on the wall.\n";
+?>
